46a11bb055c61f70bf95cd2e77ecd445eb645afe
torbjorn.nilsen@gmail.com <torbjorn.nilsen@gmail.com>
2025-09-26 23:32:40 +1000

    Encapsulate response.

--- webbrowserbridge/src/bridge/containerHttpClient.js
+++ webbrowserbridge/src/bridge/containerHttpClient.js
@@ -1,19 +1,75 @@
-import { Logger } from "coreutil_v1";
-import { ContainerAsync } from "./containerAsync";
+import { Logger, Method } from "coreutil_v1";
+import { ContainerHttpResponse } from "./containerHttpResponse";
 
 const LOG = new Logger("ContainerHttpClient");
 
 export class ContainerHttpClient {
 
-        /**
+    /**
      * 
      * @param {String} url 
      * @param {Object} params 
-     * @param {Number} connectionTimeout
-     * @param {Number} responseTimeout
+     * @param {Number} timeout
+     * @return {Promise<ContainerHttpResponse>}
      */
-    static fetch(url, params, connectionTimeout = 1000, responseTimeout = 4000) {
-        return ContainerAsync.timeout(connectionTimeout, window.fetch(url, params));
+    static async fetch(url, params, timeout = 4000) {
+        const responsePromise = fetch(url, params)
+        return ContainerHttpResponse.from(responsePromise, timeout);
     }
 
+    /**
+     * 
+     * @param {string} url 
+     * @param {File[]} files 
+     * @param {Method} progressCallbackMethod 
+     * @param {Number} timeout 
+     * @returns 
+     */
+    static async upload(url, files, authentication = null, progressCallbackMethod = null, timeout = 4000) {
+        const formData = new FormData();
+        for (const file of files) {
+            formData.append("file", file);
+        }
+
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", url);
+        xhr.timeout = timeout;
+
+        if (authentication) {
+            xhr.setRequestHeader("Authorization", "Bearer " + authentication);
+        }
+        
+        xhr.onprogress = (event) => {
+            callProgressCallbackMethod(progressCallbackMethod, event.loaded, event.total);
+        };
+        xhr.ontimeout = () => {
+            return Promise.reject("Request timed out");
+        };
+        const uploadPromise = new Promise((resolve, reject) => {
+            xhr.onload = () => {
+                if (xhr.status === 200) {
+                    resolve(xhr.response);
+                } else {
+                    reject(xhr.statusText);
+                }
+            };
+            xhr.ontimeout = () => {
+                reject("Request timed out");
+            };
+        });
+        xhr.send(formData);
+        return uploadPromise;
+    }
+
+    /**
+     * 
+     * @param {Method} progressCallbackMethod 
+     * @param {Number} loaded 
+     * @param {Number} total 
+     */
+    callProgressCallbackMethod(progressCallbackMethod, loaded, total) {
+        if (progressCallbackMethod) {
+            progressCallbackMethod.call(loaded, total);
+        }
+    }
 }
---
+++ webbrowserbridge/src/bridge/containerHttpResponse.js
@@ -0,0 +1,62 @@
+import { ContainerAsync } from "./containerAsync";
+
+export class ContainerHttpResponse {
+    
+    /**
+     * 
+     * @param {Response} response
+     */
+    constructor(response) {
+        this.response = response;
+    }
+
+    /**
+     * 
+     * @returns {Promise<Object>}
+     */
+    async json() {
+        return await this.response.json();
+    }
+
+    /**
+     * 
+     * @returns {Promise<string>}
+     */
+    async text() {
+        return await this.response.text();
+    }
+
+    /**
+     * 
+     * @returns {number}
+     */
+    get status() {
+        return this.response.status;
+    }
+
+    /**
+     * 
+     * @returns {string}
+     */
+    get statusText() {
+        return this.response.statusText;
+    }
+
+    get headers() {
+        return this.response.headers;
+    }
+
+    get ok() {
+        return this.response.ok;
+    }
+
+    /**
+     * 
+     * @param {Response} response
+     * @returns {ContainerHttpResponse}
+     */
+    static async from(response, timeout = 1000) {
+        const timeoutResponse = await ContainerAsync.timeout(timeout, response);
+        return new ContainerHttpResponse(timeoutResponse);
+    }
+}
