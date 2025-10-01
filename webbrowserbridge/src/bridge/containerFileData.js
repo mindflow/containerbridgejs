4734f00fb77e1943633bc480795f9ce547c4d597
torbjorn.nilsen@gmail.com <torbjorn.nilsen@gmail.com>
2025-10-02 02:12:32 +1000

    Refactor.

--- webbrowserbridge/src/bridge/containerFileData.js
+++ webbrowserbridge/src/bridge/containerFileData.js
@@ -5,10 +5,39 @@ export class ContainerFileData {
      * @param {File} file 
      */
     constructor(file) {
-        this.name = file.name;
-        this.type = file.type;
-        this.size = file.size;
-        this.lastModified = file.lastModified;
         this.file = file;
     }
+
+    get name() {
+        return this.file.name;
+    }
+
+    get size() {
+        return this.file.size;
+    }
+
+    get type() {
+        return this.file.type;
+    }
+
+    get lastModified() {
+        return this.file.lastModified;
+    }
+
+    /**
+     * 
+     * @returns {Promise<ArrayBuffer>}
+     */
+    async toArrayBuffer() {
+        return await this.file.arrayBuffer();
+    }
+
+    /**
+     * 
+     * @returns {Promise<string>}
+     */
+    async toBase64() {
+        const arrayBuffer = await this.toArrayBuffer();
+        return btoa(String.fromCharCode(...new Uint8Array(arrayBuffer)));
+    }   
 }
